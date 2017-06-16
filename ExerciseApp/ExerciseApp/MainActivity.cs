15151ea31ce8002edd9631b856dc881b0bcceddc
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-06-16 13:13:56 +0100

    Show total amount of press ups to the screen and implement clear button

--- ExerciseApp/ExerciseApp/MainActivity.cs
+++ ExerciseApp/ExerciseApp/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.App;
 using Android.Widget;
 using Android.OS;
+using Java.IO;
 
 namespace ExerciseApp
 {
@@ -10,6 +11,7 @@ namespace ExerciseApp
     {
         private int _pushUpsDone = 0;
         private EditText _pushUpsToAdd;
+        private TextView totalLabel;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,16 +22,35 @@ namespace ExerciseApp
 
             // Get the UI controls from the loaded layout
             _pushUpsToAdd = FindViewById<EditText>(Resource.Id.pushUpsToAdd);
+            totalLabel = FindViewById<TextView>(Resource.Id.totalLabel);
             Button addButton = FindViewById<Button>(Resource.Id.addButton);
+            Button clearButton = FindViewById<Button>(Resource.Id.clearButton);
+            
             addButton.Click += AddButtonOnClick;
+            clearButton.Click += ClearButtonOnClick;
 
-            EditText totalLabel = FindViewById<EditText>(Resource.Id.totalLabel);
             totalLabel.Text = _pushUpsDone.ToString();
         }
-
+        
         private void AddButtonOnClick(object sender, EventArgs eventArgs)
         {
             _pushUpsDone += int.Parse(_pushUpsToAdd.Text);
+            totalLabel.Text = _pushUpsDone.ToString();
+
+            SaveAmount();
+        }
+
+        private void ClearButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            _pushUpsDone = 0;
+            totalLabel.Text = _pushUpsDone.ToString();
+
+            SaveAmount();
+        }
+
+        private void SaveAmount()
+        {
+            // Add save code here
         }
     }
 }
