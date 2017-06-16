3453d022b4daec042c3af54215ca74a438ee1860
Sam Greaves <sam.greaves@meyertech.co.uk>
2017-06-16 09:22:55 +0100

    Add button clicked method to process the adding of numbers

--- ExerciseApp/ExerciseApp/MainActivity.cs
+++ ExerciseApp/ExerciseApp/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 
@@ -7,6 +8,9 @@ namespace ExerciseApp
     [Activity(Label = "ExerciseApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private int _pushUpsDone = 0;
+        private EditText _pushUpsToAdd;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -15,8 +19,17 @@ namespace ExerciseApp
             SetContentView(Resource.Layout.Main);
 
             // Get the UI controls from the loaded layout
-            EditText pushUpsToAdd = FindViewById<EditText>(Resource.Id.pushUpsToAdd);
+            _pushUpsToAdd = FindViewById<EditText>(Resource.Id.pushUpsToAdd);
             Button addButton = FindViewById<Button>(Resource.Id.addButton);
+            addButton.Click += AddButtonOnClick;
+
+            EditText totalLabel = FindViewById<EditText>(Resource.Id.totalLabel);
+            totalLabel.Text = _pushUpsDone.ToString();
+        }
+
+        private void AddButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            _pushUpsDone += int.Parse(_pushUpsToAdd.Text);
         }
     }
 }
